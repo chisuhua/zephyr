ebb08735cbb5e718eab002bc5696c9d0b988665c
chisuhua <chisuhua@gmail.com>
2020-07-29 22:00:34 +0800

    hello_world can access gem5 memory now

--- run_build.sh
+++ run_build.sh
@@ -2,7 +2,17 @@
 # -*- coding: utf-8 -*-
 
 #west build -p auto -b native_posix_64 samples/basic/blinky
+#west -v build -d build_posix -t menuconfig -p auto -b native_posix_64 samples/hello_world
 #west -v build -d build_posix -p auto -b native_posix_64 samples/hello_world
+#west -v build -d build_posix_blinky -p auto -b native_posix_64 samples/basic/blinky_pwm
+west -v build -d build_posix_gem5 -p auto -b native_posix_64_gem5 samples/hello_world
 
 # build ppu32
-west -v build -d build_ppu32 -p auto  -b ppu32 samples/hello_world
+#west -v build -d build_ppu32 -p auto  -b ppu32 samples/hello_world
+
+
+#west -v build -d build_hifive -p auto -b hifive1 samples/hello_world
+
+# tests
+#west -v build -d build_tests_posix -p auto -b native_posix_64 tests/boards/native_posix/native_tasks
+#west -v build -d build_tests_posix -p auto -b native_posix_64 tests/boards/native_posix/rtc
--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -9,26 +9,42 @@
 
 // extern void* get_gem5api_vector(char *api);
 
-typedef void (*zephyr_api_mem_access)(bool, uint32_t, bool, long int , bool);
+typedef uint32_t (*zephyr_api_mem_read)(uint64_t, bool/*=false*/);
+typedef void (*zephyr_api_mem_write)(uint64_t, uint32_t, bool/*=false*/);
+typedef void (*zephyr_api_exit_sim)();
 // typedef void* (*gem5api_vector)[];
 void* (*gem5api_vector)[10];
 
 void main(void)
 {
+zephyr_api_mem_read mem_read = (zephyr_api_mem_read)(*gem5api_vector)[0];
+zephyr_api_mem_write mem_write = (zephyr_api_mem_write)(*gem5api_vector)[1];
+zephyr_api_exit_sim exit_sim = (zephyr_api_exit_sim)(*gem5api_vector)[2];
+
 
     // gem5api_vector = (gem5api_vector)get_gem5api_vector(NULL);
-    zephyr_api_mem_access mem_access = (zephyr_api_mem_access)(*gem5api_vector)[0];
-    // zephyr_api_mem_access mem_access2 = (zephyr_api_mem_access)(*gem5api_vector)[1];
 
 	printk("Hello World Start! %s\n", CONFIG_BOARD);
 	k_sleep(K_MSEC(100));
     int cnt = 0;
     while(cnt < 10) {
         cnt++;
-	    printk("%d:Hello World!\n", cnt);
-        mem_access(false, 1000, false, 0x10000 + cnt, false);
-        // mem_access2(false, 1000, false, 0x10000, false);
+	    int32_t data = k_uptime_get_32() * cnt;
+        // int32_t data = random(100);
+        int64_t addr = 1000 + cnt*4;
+	    printk("%d:Write data %x to addr %lx !\n", cnt, data, addr);
+        mem_write(addr, data, false);
     }
-    posix_exit(0);
+
+    cnt = 0;
+    while(cnt < 10) {
+        cnt++;
+        int64_t addr = 1000 + cnt*4;
+        int32_t data = mem_read(addr, false);
+	    printk("%d:Read the data %x from addr %lx\n", cnt, data, addr);
+    }
+
+    exit_sim();
+    // posix_exit(0);
 	// printf("Hello World in printf! %s\n", CONFIG_BOARD);
 }
